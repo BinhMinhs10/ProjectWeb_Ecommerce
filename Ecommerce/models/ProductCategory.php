c1680e4fc4624aebb21e805ec4bd2573cd23918e
Muatrenbuicay <37929295+Muatrenbuicay@users.noreply.github.com>
2018-04-12 01:44:17 -0700

    Update ProductCategory.php

--- Ecommerce/models/ProductCategory.php
+++ Ecommerce/models/ProductCategory.php
@@ -5,4 +5,53 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+class ProductCategory {
+
+    private $category_id;
+    private $category_name;
+    private $parent_id;
+    private $category_order;
+
+    function __construct($category_id, $category_name, $parent_id, $category_order) {
+        $this->category_id = $category_id;
+        $this->category_name = $category_name;
+        $this->parent_id = $parent_id;
+        $this->category_order = $category_order;
+    }
+    function getCategory_id() {
+        return $this->category_id;
+    }
+
+    function getCategory_name() {
+        return $this->category_name;
+    }
+
+    function getParent_id() {
+        return $this->parent_id;
+    }
+
+    function getCategory_order() {
+        return $this->category_order;
+    }
+
+    function setCategory_id($category_id) {
+        $this->category_id = $category_id;
+    }
+
+    function setCategory_name($category_name) {
+        $this->category_name = $category_name;
+    }
+
+    function setParent_id($parent_id) {
+        $this->parent_id = $parent_id;
+    }
+
+    function setCategory_order($category_order) {
+        $this->category_order = $category_order;
+    }
+
+    
+}
+?>
+    
 
