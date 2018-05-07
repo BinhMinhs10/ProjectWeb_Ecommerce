f81b01ea97a593db678534a8ac64b0ec8f96b1e7
Bình Minh <nguyenbinhminh07101997@gmail.com>
2018-05-07 18:08:16 +0700

    miss view

--- application/views/manager/product_view.php
+++ application/views/manager/product_view.php
@@ -7,6 +7,14 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<style type="text/css">
+	    a {
+	      padding-left: 15px;
+	      padding-right: 15px;
+	      margin-left: 15px;
+	      margin-right: 15px;
+	    }
+    </style>
 </head>
 <body >
 
@@ -82,35 +90,82 @@
 				</div><br/>
 
 
-	 			<div class="container">
-	 				<div class="row">
-		 				<?php foreach ($products as $value): ?>
-							<div class="col-sm-4">
-								<div class="card card-block">
-									<img  style="height: 350px" class="card-img-top img-fluid img-thumbnail" src="<?php echo base_url().$value['image']; ?>" alt="Card images" >
-									<div class="card-body">
-										<h4 class="card-title"><?= $value['product_name'] ?></h4>
-				 						<p class="card-text category_id">Id category: <b><?= $value['category_id'] ?></b></p>
-				 						<p class="card-text company">Hãng sản xuất: <b><?= $value['company'] ?></b></p>
-				 						<p class="card-text price">Giá: <b><?= $value['price'] ?></b></p>
-				 						<p class="card-text discount">Khuyến mại: <b><?= $value['discount'] ?></b></p>
-				 						<p class="card-text quantity">Số Lượng: <b><?= $value['quantity'] ?></b></p>
-				 						<p class="card-text intro">Giới thiệu: <b><?= $value['intro'] ?></b></p>
-										<br/>
-										<p class="card-text editns"><small><a href="<?= base_url(); ?>/index.php/product/getProduct/<?= $value['product_id'] ?>" class="btn btn-warning btn-xs">Sửa sản phẩm</a></small></p>
-
-										<p class="card-text delns"><small><a href="<?= base_url(); ?>/index.php/product/product_delete/<?= $value['product_id'] ?>" class="btn btn-outline-danger btn-xs">Xóa sản phẩm</a></small></p>
-									</div>
-									
-								</div><br/>
-							</div>
-						<?php endforeach ?>
-		 			</div>	
-	 			</div><!-- end container-->
-
+	 			
+ 				<div class="row" id='postsList'>
+	 				
+	 			</div>	
+	 			
+				<div style='margin-top: 10px;' id='pagination'></div>
 	 		</div><!-- end full container-->
  		</div>
  	</div>
  	
+   
+    <script type='text/javascript'>
+	    $(document).ready(function(){
+
+	    	// Detect pagination click
+	    	$('#pagination').on('click','a',function(e){
+	       		e.preventDefault(); 
+	       		var pageno = $(this).attr('data-ci-pagination-page');
+	       		loadPagination(pageno);
+	    	});
+
+		    loadPagination(0);
+
+	    	// Load pagination
+	     	function loadPagination(pagno){
+	       		$.ajax({
+	         		url: '<?=base_url()?>index.php/product/loadRecord/'+pagno,
+	         		type: 'get',
+	         		dataType: 'json',
+	         		success: function(response){
+		            	$('#pagination').html(response.pagination);
+		            	createTable(response.result,response.row);
+	         		}
+	        	});
+	    	}
+
+		    	// Create table list
+		    function createTable(result,sno){
+		       	sno = Number(sno);
+		       	$('#postsList').empty();
+		       	
+		       	for(index in result){
+		          	var product_id = result[index].product_id;
+		          	var product_name = result[index].product_name;
+		          	var category_id = result[index].category_id;
+		          	var company =result[index].company;
+		          	var price =result[index].price;
+		          	var discount =result[index].discount;
+		          	var quantity=result[index].quantity;
+		          	var intro=result[index].intro;
+
+		          	var image = result[index].image;
+		          	// content = content.substr(0, 60) + " ...";
+		          	sno+=1;
+
+		          	var tr = '<div class="col-sm-4">';
+		          	tr += '<div class="card card-block">';
+		          	tr += '<img  style="height: 350px" class="card-img-top img-fluid img-thumbnail" src="<?php echo base_url(); ?>'+image+'" alt="Card images" >';
+		          	tr += '<p class="card-text category_id">Id category: <b>'+category_id+'</b></p>';
+		          	tr += '<p class="card-text company">Hãng sản xuất: <b>'+company+'</b></p>';
+		          	tr += '<p class="card-text price">Giá: <b>'+ price+'</b></p>';
+		          	
+		          	tr += '<p class="card-text discount">Khuyến mại: <b>'+discount+'</b></p>';
+		          	tr += '<p class="card-text quantity">Số Lượng: <b>'+quantity+'</b></p>';
+		          	tr += '<p class="card-text intro">Giới thiệu: <b>'+intro+'</b></p><br/>';
+		          	tr += '<p class="card-text editns"><small><a href="<?= base_url(); ?>/index.php/product/getProduct/'+product_id+'" class="btn btn-warning btn-xs">Sửa sản phẩm</a></small></p>';
+		          	tr += '<p class="card-text delns"><small><a href="<?= base_url(); ?>/index.php/product/product_delete/'+product_id+'" class="btn btn-outline-danger btn-xs">Xóa sản phẩm</a></small></p>';
+		          	tr += '</div></div></br>';
+		          	
+		          	  	
+		          	$('#postsList').append(tr);
+		 			
+		        }
+		        
+		    }
+	    });
+    </script>
 </body>
 </html>
