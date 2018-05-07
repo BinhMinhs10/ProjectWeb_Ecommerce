1613c548cefc5d1a758d354c949557808138fa1a
TrinhHaiSon <haisontrinh96@gmail.com>
2018-05-07 07:56:28 +0700

    client view

--- application/views/manager/sidebar.php
+++ application/views/manager/sidebar.php
@@ -42,8 +42,6 @@
     <!-- /#sidebar-wrapper -->
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Menu Toggle Script -->
     <script>
